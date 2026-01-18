83864845cf9122d06a627eb2187a2cab585be6fb
dauletnb <dauletn682@gmail.com>
2026-01-18 17:27:17 +0500

    initial commit

---
+++ src/main/java/com/emeraldgrove/EmeraldGroveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emeraldgrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeraldGroveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmeraldGroveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emeraldgrove/EmeraldGroveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emeraldgrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeraldGroveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
